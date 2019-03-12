782c0e20803411418e3d58ec23d694627688fc9d
zhanghongbo <shawnzhang@gmail.com>
2019-03-12 14:55:52 +0800

    init

---
+++ src/MyPake.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghongbo
+ * Date: 2019/3/12
+ * Time: 下午2:54
+ */
+namespace Anlewo\ApiView;
+
+class MyPake
+{
+    public function say()
+    {
+        return 'my first extension';
+    }
+}
