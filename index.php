f6c918945d2f0161abe1b49760ba75d727bf7019
zhanghongbo <shawnzhang@gmail.com>
2019-03-14 14:24:46 +0800

    1

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-
-$my = new \Anlewo\ApiView\MyPake();
-echo $my->say();
--- src/views/layouts/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zhanghongbo
- * Date: 2019/3/12
- * Time: 下午3:18
- */
-
-echo 123123;
